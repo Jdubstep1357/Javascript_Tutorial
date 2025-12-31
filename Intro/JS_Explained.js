f07d6256fdb5620b80eb477d0d7c3a562612e154
John Lyons <jwlyons2015@gmail.com>
2025-12-31 15:30:54 -0500

    First commit

---
+++ Intro/JS_Explained.js
@@ -0,0 +1,13 @@
+/* 
+
+Javascript is most popular language
+- Build web  / Mogile Apps
+Real-time web applications
+Javascript runs on browser, ie: Javascript Engine
+
+ECMAScript - Specificaion / Standards
+Javascript - Programming Language
+
+
+
+*/
