272dfcdaf55f58cf7d1d21cc0c3d5ae655ded948
John Lyons <jwlyons2015@gmail.com>
2026-01-01 17:16:07 -0500

    Constants

---
+++ Bascis/2_Constants/index.js
@@ -0,0 +1,9 @@
+// Variable can change
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+// Constant - Value of constant cannot change - error
+const interestRateFixed = 0.4;
+interestRateFixed = 0.8;
+console.log(interestRateFixed);
