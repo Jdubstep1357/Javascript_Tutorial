d0b78bcce0208e6bcacb226fb376c82c7a4f3287
John Lyons <jwlyons2015@gmail.com>
2026-01-01 17:37:34 -0500

    Objects

---
+++ Bascis/5_Objects/index.js
@@ -0,0 +1,24 @@
+// Object
+
+// Object - A variable with subcategories (properties)
+
+let name = "Mosh";
+let age = 30;
+
+// { } objct literal
+const person = {
+    // Keys are properties of objects
+    name: "John",
+    age: 30
+};
+
+
+// Dot Notation - change part of property
+person.name = "Macy";
+
+
+// Bracket notation
+let selection = "Name";
+person[selection] = "Grace";
+
+
