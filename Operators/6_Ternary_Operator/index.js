631bcaebf2681d941a39e671e96fa4aea7f5af0a
John Lyons <jwlyons2015@gmail.com>
2026-01-01 18:38:24 -0500

    Ternary_Operator

---
+++ Operators/6_Ternary_Operator/index.js
@@ -0,0 +1,9 @@
+// If a customer has more than 100 points, are 'gold'
+// Otherwise are 'silver' customer
+
+
+let points = 110
+// if points are above 100, print out gold. otherwise get silver
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
