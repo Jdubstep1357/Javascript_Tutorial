e64a131d8ece2b19c5ab87f91cfe1ee645339bc6
John Lyons <jwlyons2015@gmail.com>
2026-01-01 18:23:35 -0500

    Assignment_Operators

---
+++ Operators/2_Arithmetic_Operators/index.js
@@ -0,0 +1,28 @@
+let x = 10;
+let y = 3;
+
+// Addition Operator
+console.log(x + y);
+// Subtraction
+console.log(x - y);
+// Multiplication
+console.log(x * y);
+// Divisible
+console.log(x / y);
+// Remainder 
+console.log(x % y);
+/// Exponentiation - x to power of y
+console.log(x ** y);
+
+console.log("Increment and decrement operators: ");
+
+// Increment (++)
+// Makes value of x incremented by one first then on console
+console.log(++x);
+// Console first then x adds one
+console.log(x++);
+
+
+// Decrement operator
+console.log(--x);
+console.log(x--);
