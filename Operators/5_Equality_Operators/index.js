dedaef97e5c9622af9b2830d46316d12e166e529
John Lyons <jwlyons2015@gmail.com>
2026-01-01 18:35:04 -0500

    Equality Operator

---
+++ Operators/5_Equality_Operators/index.js
@@ -0,0 +1,15 @@
+// Equality operator
+
+// Strict Equality
+// Ensures both operands are of type and value are equal
+console.log(1 === 1);
+// False
+console.log('1' === 1);
+
+
+// Loose Equality - if types dont match convert left to right
+console.log(1 == 1);
+// True
+console.log('1' == 1);
+console.log(true == 1);
+
