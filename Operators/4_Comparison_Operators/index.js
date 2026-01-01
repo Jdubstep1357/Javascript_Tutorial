f46193dbe44d87069e331c0240859ce06b86553d
John Lyons <jwlyons2015@gmail.com>
2026-01-01 18:30:20 -0500

    Comparison Operators

---
+++ Operators/4_Comparison_Operators/index.js
@@ -0,0 +1,15 @@
+// Compare value of variable with something else
+
+let x = 1;
+
+// Relational operators
+console.log(x > 0);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+
+// Equality operators
+console.log(x === 1);
+// if not equal
+console.log(x !== 1);
