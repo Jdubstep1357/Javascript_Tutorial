d36d34236511f5bc5043874aee8caef18178b0e1
John Lyons <jwlyons2015@gmail.com>
2026-01-01 18:16:41 -0500

    Types of Functions

---
+++ Bascis/_8_Types_Of_Functions/index.js
@@ -0,0 +1,13 @@
+// performing a task
+function greet(name, lastName) {
+    console.log("Hello" + name + '' + lastName)
+}
+
+// Calculate a number
+function square(number) {
+    return number * number;
+}
+
+
+
+console.log(square(2));
