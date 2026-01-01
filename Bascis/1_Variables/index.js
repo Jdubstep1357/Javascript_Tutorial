d0e1968edadf4406f78a2b1926e6efbbae0fafad
John Lyons <jwlyons2015@gmail.com>
2026-01-01 16:58:26 -0500

    Variables

---
+++ Bascis/1_Variables/index.js
@@ -0,0 +1,17 @@
+// Use let to declare value
+let name1 = "John";
+console.log(name1);
+
+
+/* Rules 
+1) Cannot be a reversed keword
+2) Name should be meaningful and descriptive names of variables
+3) Cannot start with a number
+4) Cannot contain space or hyphen
+5a) Declare multi vars - declare on one line, seperate with comma
+5b) OR declare each variable on a single line
+*/
+
+// Camel notation first letter lower second name uppercase
+let firstName = "John";
+let lastName = "Lyons"
