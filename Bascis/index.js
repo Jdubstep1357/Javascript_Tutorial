ed666eab445079b0f81ed607c71d09ce30903abd
John Lyons <jwlyons2015@gmail.com>
2026-01-01 17:23:17 -0500

    Primitive Types

---
+++ Bascis/index.js
@@ -0,0 +1,9 @@
+// Types of Data
+
+// Primitives - Value Types
+
+let name = "John";// String literal
+let age = 30;// Number literal
+let isApproved = true; // Boolean Literal
+let firstName; // Undefined
+let selectedColr = null; // Useful when clearning value of var
