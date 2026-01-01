e65bdb050d7b76d95bc7a8d2bc0bcb7a583a1943
John Lyons <jwlyons2015@gmail.com>
2026-01-01 17:29:03 -0500

    Dynamic_Typing

---
+++ Bascis/4_Dynamic_Typing/index.js
@@ -0,0 +1,14 @@
+// Static vs Dynamic
+
+// Statically typed - type of variable cannot be changed once set
+// Dynamic - type of variables determined at runtime
+
+// typeof in terminal shows type of value 
+
+
+let name = "John";// String literal
+let age = 30;// Number literal
+let isApproved = true; // Boolean Literal
+let firstName; // Undefined
+let selectedColr = null; // Useful when clearning value of var
+
