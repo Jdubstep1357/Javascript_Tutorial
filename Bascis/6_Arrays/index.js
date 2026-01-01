3857824ac410426d2d7a59810b9d170c43d1d68c
John Lyons <jwlyons2015@gmail.com>
2026-01-01 17:43:21 -0500

    Arrays

---
+++ Bascis/6_Arrays/index.js
@@ -0,0 +1,11 @@
+// Arrays - Data structure to represent list of items
+
+// Array Literal - Empty Array
+// Each part of array is referenced 0, 1
+let SelectedColors = ['red', 'blue'];
+// Add to array
+selectedColors[2] = 2;
+
+
+// References red
+console.log(selectedColors);
