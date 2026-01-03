00bd1ea3f6f8c5104b129a406ffe58cbf2e428d0
John Lyons <jwlyons2015@gmail.com>
2026-01-03 13:36:07 -0500

    11_LandscapePortrait

---
+++ Control_Flow/11_LandscapePortrait/index.js
@@ -0,0 +1,10 @@
+// takes 2 parameters, width, he
+// ight and returns true if 
+// width > height, otherwise false
+
+let landscape = isLandscape(5, 4)
+console.log(landscape);
+
+function isLandscape(width, height) {
+    return (width > height);
+}
