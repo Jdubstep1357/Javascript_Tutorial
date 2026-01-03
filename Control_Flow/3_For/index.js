6a98ac635262f8e6d244a92c8ac1e276929f0c27
John Lyons <jwlyons2015@gmail.com>
2026-01-03 12:25:01 -0500

    For loops

---
+++ Control_Flow/3_For/index.js
@@ -0,0 +1,24 @@
+// Loops - Repeat an action number of times
+
+/* Types of loops
+for
+while
+do... while
+for... in
+for... of
+*/
+
+
+
+// 3 Statements
+// 1. Initial statement; 2. condition 3. increment expression
+for (let i = 1; i <= 5; i++) {
+    // checking if numbers are odd
+    if (i % 2 !== 0) console.log(i);
+}
+
+// Show numbers in reverse order
+for (let i = 5; i >= 1; i--) {
+    // checking if numbers are odd
+    if (i % 2 !== 0) console.log(i);
+}
