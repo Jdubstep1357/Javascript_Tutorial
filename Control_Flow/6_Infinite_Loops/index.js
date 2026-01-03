9a8ad5a93e12dbe9a365d562a2339962978c494f
John Lyons <jwlyons2015@gmail.com>
2026-01-03 12:42:22 -0500

    Infinite Loops

---
+++ Control_Flow/6_Infinite_Loops/index.js
@@ -0,0 +1,21 @@
+let i = 0;
+
+// 1st example
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+
+// 2nd example
+while (true) {
+
+}
+
+
+let x = 0;
+do {
+
+} while (x < 5);
+
+for (let i = 0; i > 10;) console.log("Crash your computer")
