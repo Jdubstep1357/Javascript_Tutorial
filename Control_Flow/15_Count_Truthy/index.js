d3981b5f8af8407a7889406dd23f4b68e7a9d6f9
John Lyons <jwlyons2015@gmail.com>
2026-01-04 12:41:42 -0500

    Count truthy

---
+++ Control_Flow/15_Count_Truthy/index.js
@@ -0,0 +1,28 @@
+/// Takes value that retrieves truthy values
+// It's an array
+
+/* Falsey Values:
+
+undefined
+null
+''
+false
+0
+Not a number
+
+*/
+
+
+const array = [0, null, undefined, '', 2, 3];
+
+console.log(countTruthy(array));
+
+function countTruthy(array) {
+    let count = 0;
+    for (let value of array)
+        if (value)
+            count++;
+    return count;
+
+}
+
