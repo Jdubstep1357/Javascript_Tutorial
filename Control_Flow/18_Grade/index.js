789c96dbcc0fc8ffec6d806f4424fc2e3003228e
John Lyons <jwlyons2015@gmail.com>
2026-01-07 23:29:57 -0500

    18_Grade

--- Control_Flow/18_Grade/index.js
+++ Control_Flow/18_Grade/index.js
@@ -1,16 +1,46 @@
-console.log(sum(10));
+// Calculate the grades of a student
+// pass array and calulate grade
+
+// averages grade
 
 
-// function returns all multiplse of 3 and 5 from 0  up to this limit
-// Ex: What are multiples of 3 and 5 up to 10?
+// average is 80 + 80 + 50
+// Output: C
 
-// Ex: Multiples of 3(3,6,9)
-// Multples of 5: (5, 10)
-// Output: 33
+// 1 - 59: F
+// 60 - 69: D
+// 70 - 79: C
+// 80 - 89: B
+// 90 - 100: A
 
-function sum(limit) {
-    if (i % 3 === 0 || i % 5 === 0)
-        sum += i;
 
-    return sum;
+const marks = [90, 90, 75];
+
+console.log(calculateGrade(marks)); g
+function calculateGrade(marks) {
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    const average = sum / marks.length;
+
+    if (average < 60)
+        console.log("F");
+    else if (average <= 70)
+        console.log("D");
+    else if (average <= 80)
+        console.log("C");
+    else if (average <= 90)
+        console.log("B");
+    else if (average <= 100)
+        console.log("A")
+    else
+        console.log("Error");
+
+
+
+    console.log(average);
+
 }
---
+++ Control_Flow/18_Grade/index_final.js
@@ -0,0 +1,22 @@
+const marks = [90, 90, 75];
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks) {
+
+    if (average < 60) return "F";
+    if (average < 70) return "D";
+    if (average < 80) return "C";
+    if (average < 90) return "B";
+    return "A";
+
+
+}
+
+function calculateAverage(array) {
+    let sum = 0;
+    for (let value of arra)
+        sum += value;
+    let average = sum / average.length;
+    return average;
+}
