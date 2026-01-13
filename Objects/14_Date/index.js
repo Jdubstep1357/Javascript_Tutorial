23c836e256a305489a42601cdb61ef20ff19816e
John Lyons <jwlyons2015@gmail.com>
2026-01-12 22:50:58 -0500

    14_Date

---
+++ Objects/14_Date/index.js
@@ -0,0 +1,8 @@
+const now = new Date();
+const date1 = new Date('May 11 2018 09:00');
+// Month is 0 based 0 = january december = 11
+// year, month, day of month, hour, minute
+const date2 = new Date(2018, 4, 11, 9, 0);
+
+// returns day of month
+now.setFullYear(2017);
