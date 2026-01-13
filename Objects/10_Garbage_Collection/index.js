8d9abedb182aa0eb6e420bf4f41f973f5596d6c1
John Lyons <jwlyons2015@gmail.com>
2026-01-12 21:57:40 -0500

    10_Garabe_Collection

---
+++ Objects/10_Garbage_Collection/index.js
@@ -0,0 +1,7 @@
+// memory allocated to object
+let circle = {};
+console.log(circle);
+
+// garable collector - finds 
+// find the variables or constants that are no longer used
+// Allocate memory later
