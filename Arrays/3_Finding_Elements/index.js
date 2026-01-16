3d6a09ff676a6901a3cfcece8f527e3d03767446
John Lyons <jwlyons2015@gmail.com>
2026-01-15 20:12:08 -0500

    3_Finding_Elements(Primitives)

---
+++ Arrays/3_Finding_Elements/index.js
@@ -0,0 +1,18 @@
+// finding elements in array
+
+const numbers = [1, 2, 3, 1, 4];
+
+// finds index array. 0 is first place
+// if string, print out -1
+console.log(numbers.indexOf(3));
+// Output
+console.log(numbers.lastIndexOf(1));
+console.log(numbers.indexOf(1, 2));
+// shows index of array
+// returns true
+console.log(numbers.indexOf(1, 2)).log(numbers.indexOf(1) !== -1);
+
+// shows first number on left
+console.log(numbers.indexOf(1));
+// shows where to start index, ie: at position 2 (3)
+console.log(numbers.indexOf(1, 2))
