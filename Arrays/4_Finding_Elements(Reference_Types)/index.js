25cd6ba112d0a680f25d2806aa6aad232653bcba
John Lyons <jwlyons2015@gmail.com>
2026-01-17 11:11:33 -0500

    4_Finding_Elements

---
+++ Arrays/4_Finding_Elements(Reference_Types)/index.js
@@ -0,0 +1,19 @@
+const courses = [
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' },
+];
+
+// makes false
+// console.log(courses.includes({ id: 1, name: 'a' }));
+
+// callback function
+const course = courses.find(function (course) {
+    course.name === 'a';
+});
+
+// displays index of referenced course. will return -1 if undefined
+const courseIndex = course.findIndex(function (course) {
+    return course.name === 'a';
+})
+
+console.log(course);
