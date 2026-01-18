55c5d44516fd4ba43750dc28728f4acd809aae95
John Lyons <jwlyons2015@gmail.com>
2026-01-17 19:30:02 -0500

    11_Joining_Arrays

---
+++ Arrays/11_Joining_Array/index.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+
+// join
+const joined = numbers.join(',');
+console.log(joined);
+
+// Split method 
+const message = 'This is my first message';
+const parts = message.split('  ');
+console.log(parts);
+
+// combine top part
+const combined = parts.join('-');
+console.log(combined);
