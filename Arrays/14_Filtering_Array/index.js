f83311cc296fbabbe495cdc3e8a3bb1245d94a6f
John Lyons <jwlyons2015@gmail.com>
2026-01-17 20:00:20 -0500

    14_Filtering_Array

---
+++ Arrays/14_Filtering_Array/index.js
@@ -0,0 +1,8 @@
+const numbers = [1, -1, 2, 3];
+
+// 3 parameters: value, index, array
+// => is arrow function - n is number
+const filtered = numbers.filter(n => n >= 0);
+
+
+console.log(filtered);
