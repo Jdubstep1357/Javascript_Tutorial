d3276e1c44edb16758d7af00193bc51b8acc0f91
John Lyons <jwlyons2015@gmail.com>
2026-01-25 19:57:40 -0500

    9_Let_VsVar

---
+++ Functions/8_Local_Global copy/9_Let_VsVar.js
@@ -0,0 +1,9 @@
+// Let is only defined inside of scope. more strict
+// var can be used outside of scope
+
+/* 
+Ex: for (var i = 0; i < 5; i++)
+        console.log(i)
+
+    console.log(i) - DOES NOT work with let only works with var i = 0
+*/// let only works in block. will not work outside of scope
---
+++ Functions/8_Local_Global/index.js
@@ -0,0 +1,11 @@
+const message = 'hi';
+
+console.log(message);
+
+// scope of a variable determines where variable is accessible
+// when declare with let or const limited to blog
+
+function start() {
+    const message = 'hi';
+
+}
